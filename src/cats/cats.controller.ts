a65af58579af6933ed9d1ac786798b30a9d56036
lru0409 <lru040409@gmail.com>
2025-11-13 21:48:21 +0900

    feat: add cats module

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Post, Get, Body, Param } from '@nestjs/common';
+import { CreateCatDto } from './dto/create-cat.dto';
+import { CatsService } from './cats.service';
+import { Cat } from './interfaces/cat.interface';
+
+@Controller('cats')
+export class CatsController {
+  constructor(private catsService: CatsService) {}
+
+  @Post()
+  create(@Body() createCatDto: CreateCatDto) {
+    this.catsService.create(createCatDto);
+  }
+
+  @Get()
+  findAll(): Cat[] {
+    return this.catsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `Cat #${id}`;
+  }
+}
