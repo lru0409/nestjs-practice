3ec4165049e19f714a4587729527f71107779e11
lru0409 <lru040409@gmail.com>
2025-11-19 08:39:04 +0900

    feat: add app controller and module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
 import { CatsModule } from './cats/cats.module';
+import { UserModule } from './user/user.module';
+import { PostModule } from './post/post.module';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
+import { AppController } from './app.controller';
 
 @Module({
-  imports: [CatsModule],
+  imports: [
+    CatsModule,
+    UserModule,
+    PostModule,
+    ConfigModule.forRoot(),
+    PrismaModule,
+  ],
+  controllers: [AppController],
 })
 export class AppModule {}
