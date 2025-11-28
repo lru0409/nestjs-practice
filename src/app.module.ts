837984788292458e41e73087d7806e1f6668c758
rowoonleee <lru040409@gmail.com>
2025-11-28 22:00:00 +0900

    feat: add logger middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+
+import { LoggerMiddleware } from './logger.middleware';
 import { CatsModule } from './cats/cats.module';
 import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
@@ -16,4 +18,8 @@ import { AppController } from './app.controller';
   ],
   controllers: [AppController],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
