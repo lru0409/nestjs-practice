d1ce9a97132a631d33f9e0b61f34cf6da0e4b84a
rowoonleee <74223246+lru0409@users.noreply.github.com>
2025-12-03 09:21:40 +0900

    feat: add temp logger and timeout interceptors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { ValidationPipe } from '@nestjs/common';
 
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './http-exception.filter';
+import { LoggerInterceptor } from './logger.interceptor';
+import { TimeoutInterceptor } from './timeout.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +15,7 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+  app.useGlobalInterceptors(new LoggerInterceptor(), new TimeoutInterceptor());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
