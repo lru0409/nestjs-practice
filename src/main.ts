9cc60d12a74c87bd3d1ba871fa79090a444c9193
rowoonleee <74223246+lru0409@users.noreply.github.com>
2025-12-09 15:46:54 +0900

    feat: add swagger configuration

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
@@ -9,18 +10,30 @@ import { PrismaExceptionInterceptor } from './common/interceptors/prisma-excepti
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalFilters(new HttpExceptionFilter());
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
       whitelist: true,
     }),
   );
+
   app.useGlobalInterceptors(
     new LoggerInterceptor(),
     new TimeoutInterceptor(),
     new PrismaExceptionInterceptor(),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS Practice')
+    .setDescription('The NestJS Practice API description')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
