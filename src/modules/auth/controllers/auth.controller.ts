d9cf4dfd85f6f932ebd7a8e0907ae391b91a7985
lru0409 <lru040409@gmail.com>
2026-01-28 22:52:17 +0900

    feat: bypass AuthGuard using Public decorator

--- src/modules/auth/controllers/auth.controller.ts
+++ src/modules/auth/controllers/auth.controller.ts
@@ -11,18 +11,20 @@ import {
 
 import { AuthService } from '../services/auth.service';
 import { AuthGuard } from '@/common/guards/auth.guard';
+import { Public } from '@/common/decorators/public.decorator';
 
+@UseGuards(AuthGuard)
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @HttpCode(HttpStatus.OK)
   @Post('login')
+  @Public()
   signIn(@Body() signInDto: { username: string; password: string }) {
     return this.authService.signIn(signInDto.username, signInDto.password);
   }
 
-  @UseGuards(AuthGuard)
   @Get('profile')
   getProfile(@Request() req: Request & { user: Record<string, unknown> }) {
     return req.user;
